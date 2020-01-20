79d3d90300b9474fd1a6505cbefc0d64d5d3b604
GxHam <gaoxiang90@gmail.com>
2020-01-20 16:32:42 +0000

    Added color manager.

--- analysis/api_plot_org.py
+++ analysis/api_plot_org.py
@@ -4,6 +4,12 @@ import os
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
+import seaborn as sns
+import colorsys
+from collections import OrderedDict
+
+from api_utils import make_path_if_not_exists
+
 
 class GridFig:
     """Handles gridded figures."""
@@ -73,13 +79,13 @@ class GridFig:
             row_idx = (self.idx // self.cols)
             col_idx = (self.idx % self.cols)
 
-            print(row_idx, col_idx)
+            # print(row_idx, col_idx)
             ax = self.get_ax(row_idx, col_idx)
         else:
             row_idx = (self.idx % self.rows)
             col_idx = (self.idx // self.rows)
 
-            print(row_idx, col_idx)
+            # print(row_idx, col_idx)
             ax = self.get_ax(row_idx, col_idx)
         self._increment()
         return ax
@@ -105,5 +111,112 @@ class GridFig:
         """Private function to increase the internal idx counter."""
         self.idx = self.idx + 1
         if self.idx == self.rows * self.cols:
-            print("Looping")
+            # print("Looping")
+            self.idx = 0
+
+
+class GroupManager:
+    def __init__(self, group_list):
+        self.group_list = group_list
+        print(self.group_list)
+        self.info_dict = OrderedDict()
+        self.index = 0
+        self.color_list = [
+            "Blues", "Oranges", "Greens", "Reds", "Purples", "Greys"]
+        set_vals = sorted(set(group_list), key=group_list.index)
+        print(set_vals)
+        import numpy as np
+        if len(set_vals) > len(self.color_list):
+            start_vals = np.arange(0.0, 2.5, 2.5 / (len(set_vals) - 0.99))
+            for set_v, start_v in zip(set_vals, start_vals):
+                self.info_dict[set_v] = ColorManager(
+                    group_list.count(set_v), "sns_helix", start=start_v)
+        else:
+            start_vals = self.color_list[:len(set_vals)]
+            for set_v, start_v in zip(set_vals, start_vals):
+                self.info_dict[set_v] = ColorManager(
+                    group_list.count(set_v), "sns", sns_style=start_v)
+        print(self.info_dict)
+
+    def get_next_color(self):
+        out = self.info_dict[self.group_list[self.index]].get_next_color()
+        self._increment()
+        return out
+
+    def _increment(self):
+        self.index += 1
+        if self.index == len(self.group_list):
+            self.index = 0
+
+    def test_plot(self):
+        from scipy.stats import norm
+        import numpy as np
+        fig, ax = plt.subplots()
+        x_axis = np.arange(-15, 5, 0.001)
+        std_devs = np.arange(0.8, 3, 2.20 / len(self.group_list))
+        for sd in std_devs:
+            ax.plot(
+                x_axis, norm.pdf(x_axis, -sd * 2, sd),
+                color=self.get_next_color())
+        sns.despine(top=True, bottom=True, right=True, left=True)
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        fig.savefig("test.png", dpi=400)
+
+
+class ColorManager:
+    def __init__(self, num_colors, method="sns", **kwargs):
+        self.num_colors = num_colors
+        if method == "sns":
+            sns_style = kwargs.get("sns_style", None)
+            self.create_sns_palette(s_type=sns_style)
+        elif method == "rgb":
+            self.create_rgb()
+        elif method == "sns_helix":
+            start = kwargs.get("start", 0)
+            self.create_sns_helix(start)
+        else:
+            raise ValueError("{} not recognised method".format(
+                method))
+        self.idx = 0
+
+    def create_rgb(self):
+        N = self.num_colors
+        HSV_tuples = [(x * 1.0 / N, 0.5, 0.5) for x in range(N)]
+        RGB_tuples = map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples)
+        self.colors = list(RGB_tuples)
+
+    def create_sns_palette(self, s_type=None):
+        self.colors = sns.color_palette(s_type, self.num_colors)
+
+    def create_sns_helix(self, start):
+        self.colors = sns.cubehelix_palette(self.num_colors, start)
+
+    def get_next_color(self):
+        result = self.colors[self.idx]
+        self._increment()
+        return result
+
+    def get_color(self, index):
+        return self.colors[index]
+
+    def test_plot(self):
+        from scipy.stats import norm
+        import numpy as np
+        fig, ax = plt.subplots()
+        x_axis = np.arange(-15, 5, 0.001)
+        std_devs = np.arange(0.8, 3, 2.20 / self.num_colors)
+        for sd in std_devs:
+            ax.plot(
+                x_axis, norm.pdf(x_axis, -sd * 2, sd),
+                color=self.get_next_color())
+        sns.despine(top=True, bottom=True, right=True, left=True)
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        fig.savefig("test.png", dpi=400)
+
+    def _increment(self):
+        """Private function to increase the internal idx counter."""
+        self.idx = self.idx + 1
+        if self.idx == self.num_colors:
             self.idx = 0
